cf93735b87ecb339b862119f61e4bf97cf3fb491
Anna Apkhanova <anna.karaseva.91@inbox.ru>
2022-06-25 21:10:51 +0300

    change route

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/', cardRoute);
 // });
 
 app.use((req, res, next) => {
-  res.status(404).send('Route is not defauned!')
+  res.status(404).send({message: 'Route is not defauned!'})
 });
 
 app.listen(PORT, () => {
--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const {postCard, getCards, getCardById, addLike, deleteLike} = require('../controllers/cards');
+const {postCard, getCards, deleteCardById, addLike, deleteLike} = require('../controllers/cards');
 
 router.post('/cards', postCard);
 router.get('/cards', getCards);
-router.get('/cards/:id', getCardById);
+router.delete('/cards/:id', deleteCardById);
 router.put('/cards/:cardId/likes', addLike);
 router.delete('/cards/:cardId/likes', deleteLike);
 
