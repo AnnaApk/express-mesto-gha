c02670e640175914cc68914fc68905916f02fedf
Anna Apkhanova <anna.karaseva.91@inbox.ru>
2022-06-25 23:11:52 +0300

    refull

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { ObjectId } = require('mongoose').Types;
 
 module.exports.postUser = (req, res) => {
   const { name, about, avatar } = req.body;
@@ -21,22 +22,37 @@ module.exports.getUsers = (req, res) => {
     .catch(err => res.status(500).send({message: err.message}));
 };
 
+// module.exports.getUserById = (req, res) => {
+//   User.findById(req.params.id)
+//     .then(user => {
+//       if (user === null) {
+//         res.status(404).send({message: 'Пользователь не найден!'})
+//         return;
+//       }
+//       res.send(user)})
+//     .catch(err => {
+//       if (err.name === 'ValidationError') {
+//         res.status(400).send({message: 'Данные пользователя не верны!'})
+//         return;
+//       }
+//       res.status(500).send({message: err.message})
+//     }
+//   );
+// };
+
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
     .then(user => {
-      if (user === null) {
+      if (err.name === 'ValidationError' || !ObjectId.isValid(user._id)) {
+        res.status(400).send({message: 'Данные пользователя не верны!'})
+        return;
+      }
+      if (!user) {
         res.status(404).send({message: 'Пользователь не найден!'})
         return;
       }
       res.send(user)})
-    .catch(err => {
-      if (err.name === 'ValidationError') {
-        res.status(400).send({message: 'Данные пользователя не верны!'})
-        return;
-      }
-      res.status(500).send({message: err.message})
-    }
-  );
+    .catch(err => res.status(500).send({message: err.message}));
 };
 
 module.exports.patchUser = (req, res) => {
